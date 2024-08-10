1f6390ced15c1b6b40c529ea554621c605afbd06
Mandar007007 <teamm2d07@gmail.com>
2024-08-10 06:34:50 +0530

    User and Auction Model and socket server created

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "config/config.env" });
+const Auction = require("./models/Auction");
+const path=require("path");
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+const server = http.createServer(app);
+
+io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on('connection', (socket) => {
+    
+})
+
+
+module.exports = server
