a4cab1fc2b1461ffca6cc15bfca7d22c4c2a9295
Akshay-vaghasiya <akshayvaghasiya3636@gmail.com>
2024-08-10 10:58:25 +0530

    Redux and navbar implemented

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Store from "./store";
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { extendTheme } from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#131313",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+    <Provider store={Store}>
+        <App />
+    
+
+
+
+        </Provider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
